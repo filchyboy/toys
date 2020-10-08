5766af97d1387df9ccec8a0a107f05bcb4a80929
Chris Filkins <filchyboy@gmail.com>
2020-10-07 18:13:50 -0700

    Added circle code

---
+++ geometric_shapes/circle.py
@@ -0,0 +1,9 @@
+import math
+
+def print_area_of_circle(radius):
+    area = math.pi * radius * radius
+    print(f"The area of a the circle is {area}")
+
+print_area_of_circle(12.4)
+
+# print(math.pi)
